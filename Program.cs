4281f16b9ad0ad833cb918cf2ffac328b52df1ce
Furkancan <bektasfurkancan@gmail.com>
2022-02-15 19:13:32 +0300

    commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace while_foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //While
+            // 1 den başlayarak consoledan girilen sayı kadar (sayı dahil) ortalama hesaplama 
+            Console.WriteLine("Bir sayı giriniz");
+            int sayı = int.Parse(Console.ReadLine());
+            int sayac = 1;
+            int toplam = 0;
+            while(sayac <= sayı)
+            {
+                toplam += sayac;
+                sayac ++;
+            }
+            Console.WriteLine(toplam/sayı);
+
+            // 'a' dan 'z' ye kadar tüm harfleri console yazdır
+            char karakter = 'a';
+            while (karakter <= 'z')
+            {
+               Console.Write(karakter);
+               karakter ++; 
+            }
+            Console.WriteLine("***Foreach***");
+            string[] arabalar = {"BMW,FORD,TOYATA,NİSSAN"};
+            foreach (var araba in arabalar)
+            {
+                Console.WriteLine(araba);
+            }
+        }
+    }
+}
